c30bf77eaace63b86c827857966b313751adba8f
freecat <kenkencancan2002@gmail.com>
2021-01-02 17:19:37 +0900

    code updated with unorderd_map

--- hash/unfinished.cpp
+++ hash/unfinished.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <unordered_map>
 
 using namespace std;
 
@@ -8,6 +9,33 @@ string solution(vector<string> participant, vector<string> completion) {
     string answer = "";
 
     cout<<"test"<<'\n';
+    
+    unordered_map<string, int> strMap;
+    for(auto elem : completion)
+    {
+        if(strMap.end() == strMap.find(elem))
+            strMap.insert(make_pair(elem, 1));
+        else
+            strMap[elem]++;
+    }
+
+    for(auto elem : participant)
+    {
+        if(strMap.end() == strMap.find(elem))
+        {
+            answer = elem;
+            break;
+        }
+        else
+        {
+            strMap[elem]--;
+            if(strMap[elem] < 0)
+            {
+                answer = elem;
+                break;
+            }
+        }
+    }
 
     return answer;
 }
