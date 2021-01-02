d4df0acea79622dab6b424496c2fd643f9a99f99
freecat <kenkencancan2002@gmail.com>
2021-01-02 15:43:58 +0900

    test comit1

--- hash/unfinished.cpp
+++ hash/unfinished.cpp
@@ -1,9 +1,13 @@
 #include <string>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
 string solution(vector<string> participant, vector<string> completion) {
     string answer = "";
+
+    cout<<"test"<<'\n';
+
     return answer;
 }
